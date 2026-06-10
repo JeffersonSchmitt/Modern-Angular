015add82dea0013cb9870b28f7fceaa08176bee4
Jefferson Schmitt <jeffersonschmitt@live.com>
2026-06-10 16:52:34 -0300

    Conditional Rendering with @if and @else

--- src/app/products/card-product/card-product.ts
+++ src/app/products/card-product/card-product.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { Product } from '../../interface/products/product';
 @Component({
   selector: 'app-card-product',
   imports: [MatCardModule,MatButtonModule],
   templateUrl: './card-product.html',
   styleUrl: './card-product.scss',
 })
-export class CardProduct {}
+export class CardProduct {
+  readonly product = input.required<Product>();
+   readonly addButtonLabel = input('Add to Cart');
+}
